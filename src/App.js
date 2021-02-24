73c920ea7b2a8ffb350e2339791fe10fc13ffdfc
arafatyamin <mdh631987@gmail.com>
2021-02-24 17:01:59 +0600

    my first commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <p>I am a react person</p>
+        <p>start </p>
+        <person></person>
       </header>
     </div>
   );
+};
+function person() {
+  return <h2>Name: Yamin Arafat</h2>
 }
 
 export default App;
